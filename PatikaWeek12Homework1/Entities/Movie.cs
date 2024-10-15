156ad33e2f4297cced87e13980c582122c7334f9
ayşegül güleş <159700271+aysegulgules@users.noreply.github.com>
2024-10-16 02:29:32 +0300

    Create Movie.cs

---
+++ PatikaWeek12Homework1/Entities/Movie.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatikaWeek12Homework1.Entities
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Genre { get; set; }
+
+        [Required]
+        public int ReleaseYear { get; set; }
+    }
+}
