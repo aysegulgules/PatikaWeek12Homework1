03644b58d22d04ea57380c59827ae12d10e63375
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-10-16 01:58:43 +0300

    Add project files.

---
+++ PatikaWeek12Homework1/Context/PatikaFirstDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatikaWeek12Homework1.Models;
+
+namespace PatikaWeek12Homework1.Context
+{
+    public class PatikaFirstDbContext: DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=LAPTOP-748G4HT2;database=PatikaCodeFirstDb1;Trusted_Connection=true;TrustServerCertificate=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
